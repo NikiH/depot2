aa64cfeda0e9bcfcd7bfb666d47522c37e2ee934
NikiH <niki.hausammann@gmail.com>
2012-05-14 21:07:22 +0200

    scaffold products

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Depot::Application.routes.draw do
+  resources :products
+
+  get "say/hello"
+
+  get "say/goodbye"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
