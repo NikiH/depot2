4fb4b51677e82f8d72f9e9706f22b35e38fa74d0
NikiH <niki.hausammann@gmail.com>
2012-05-29 14:25:52 +0200

    fixed attr_accessible

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,5 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :product, :product_id, :id, :updated_at, :cart_id, :created_at
+  attr_accessible :product, :product_id, :id, :updated_at, :cart_id, :created_at, :quantity
   belongs_to :product
   belongs_to :cart
 end
