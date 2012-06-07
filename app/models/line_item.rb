897a070fbcb0207a4cb032d48d927d942a791936
NikiH <niki.hausammann@gmail.com>
2012-06-08 00:38:05 +0200

    fucking ruby

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,4 +2,8 @@ class LineItem < ActiveRecord::Base
   attr_accessible :product, :product_id, :id, :updated_at, :cart_id, :created_at, :quantity
   belongs_to :product
   belongs_to :cart
+  
+  def total_price
+    product.price * quantity
+  end
 end
